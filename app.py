81771f5d69097f04add6b9f6a34b9b1520545d8a
keniamelissaas-art <keniamelissa.as@gmail.com>
2026-06-20 23:59:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # app.py
 import streamlit as st
 from modulos.login import login
-from modulos.clientes import agregar_cliente
-from modulos.productos import agregar_producto
-from modulos.pedidos import agregar_pedido
+from modulos.clientes import mostrar_cliente
+from modulos.productos import mostrar_producto
+from modulos.pedidos import mostrar_pedido
 # Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
   # Mostrar el menú lateral
